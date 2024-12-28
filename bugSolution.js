37039f0da662f1f85be5e20e94d81a8cc67de397
BugHunter-X <askzenai@gmail.com>
2024-12-28 04:10:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/about.js
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+```
